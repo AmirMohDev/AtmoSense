9d1cbec0beefcbd36909dac3223edbf9bec54fae
amir <contact.amir.51@gmail.com>
2025-06-26 10:24:49 -0700

    creating an intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,34 @@
+import "./intro.css";
+import weatherlogo from "../../assets/image/weathericon.png";
+import { Link } from "react-router-dom";
+import Introtext from "./Introtext";
 const Intro = () => {
-    return ( 
+  return (
     <>
-    
+      <div className="background w-screen h-screen">
+        <div className="flex justify-center flex-col items-center">
+          <div className="w-[200px] lg:w-[250px] flex justify-center ml-[50px]  top-[60px]">
+            <img src={weatherlogo} alt="" />
+          </div>
+          <div className="flex flex-col ">
+            <h2 className="font-[600] mt-[40px] text-[60px] lg:text-[70px] text-white flex justify-center">
+            Atmosense
+            </h2>
+
+            <div>
+              <Introtext/>
+            </div>
+          </div>
+        </div>
+        <Link className="flex justify-center items-end mt-[100px]" to={"/weather"}>
+          <button className="  justify-center w-fit px-[10px] text-white text-[20px] flex items-center border-[1px] border-white rounded-[500px] mt-[50px] gap-[10px] transition-all duration-[1000ms] hover:bg-[#0e8df4]">
+            Start Exploring the
+            <img className="w-[60px] relative bottom-[3px]" src={weatherlogo} alt="" />
+          </button>
+        </Link>
+      </div>
     </>
- );
-}
- 
+  );
+};
+
 export default Intro;
